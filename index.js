adb7950f2083d16163d53563fc502e28cc454d01
Elvin Yung <elvin@elvinyung.com>
2015-10-27 18:32:17 -0400

    Add metadata

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-module.exports = require('./lib');
+'use strict';
+
+var math = require('mathjs');
+
+var ERROR_RESPONSE = ':1234: :question: :question:';
+
+var main = function main(argv, response, logger) {
+  var respBody = ERROR_RESPONSE;
+
+  try {
+    var expression = argv.slice(1).join('');
+    var result = math.eval(expression);
+
+    if (typeof result === 'function') {
+      logger.error('Somehow got a function in a scalk eval');
+    }
+    else {
+      respBody = math.eval(expression).toString();
+    }
+  } catch(e) {
+    logger.error(e);
+  }
+
+  response.end(respBody);
+};
+
+main.metadata = require('./plugin');
+
+module.exports = main;
